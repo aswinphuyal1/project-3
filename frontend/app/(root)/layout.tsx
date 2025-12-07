3097b4cabe5511c4e986db229cf97ec9ae89e6bc
aswin <phuyalaswin651@gmail.com>
2025-12-07 14:04:23 +0545

    diff pages folder created

--- frontend/app/(root)/layout.tsx
+++ frontend/app/(root)/layout.tsx
@@ -1,9 +1,15 @@
+import Navbar from "@/components/Navbar";
 import React from "react";
+ import {ToastContainer, toast} from 'react-toastify';
+// Replace this with your actual authentication logic
+const isLoggedIn = false; // e.g., get from context, cookies, etc.
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
+      <Navbar />
       {children}
+     <ToastContainer />
     </div>
   );
 };
