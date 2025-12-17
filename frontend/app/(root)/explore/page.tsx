33627e62f6f868a5105f2dfa85c00b6fc4cd26d9
aswin <phuyalaswin651@gmail.com>
2025-12-17 11:25:06 +0545

    added recommendedForYou components

--- frontend/app/(root)/explore/page.tsx
+++ frontend/app/(root)/explore/page.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import RecommendedForYou from '@/components/Recommended'
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+    <RecommendedForYou/>
+    </div>
   )
 }
 
