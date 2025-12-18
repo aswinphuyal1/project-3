116242552f1f9ed2b9e5e0b846feb90f8e8af5ee
aswin <phuyalaswin651@gmail.com>
2025-12-18 11:06:29 +0545

    changed package.json so we can use impoert in code

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,27 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
 
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+
+//app config
 const app = express();
 const port = process.env.PORT || 4000;
+
+;
+
+//middlewares
 app.use(express.json());
 app.use(cors());
+/*
+CORS allows your API to be accessed from different domains.
+Without it, browsers block requests from frontends on different ports/domains.
+Example: Frontend on localhost:3000 can call backend on localhost:4000
+*/
+
+
 app.get("/", (req, res) => {
-  res.send("APi working");
+  res.send("Api working");
 });
-app.listen(port, () => console.log("server started on port :", port));
+
+app.listen(port, () => console.log("serve started on port :", port));
