378239e25064bef2e52b0e007db4e222c31fcfd3
aswin <phuyalaswin651@gmail.com>
2025-12-18 21:32:19 +0545

    done with my profile components

--- frontend/app/(root)/profile/page.tsx
+++ frontend/app/(root)/profile/page.tsx
@@ -2,12 +2,13 @@
 import { ProfileAnalyticsChart } from '@/components/ProfileAnalyticsChart';
 import ProfileBanner from '@/components/ProfileBanner'
 import WelcomeCard from '@/components/welcome'
+import WelcomeForProfile from '@/components/welcomeforprofile';
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      <WelcomeCard/>
+      <WelcomeForProfile/>
       <ProfileBanner/>
       <ProfileAnalyticsChart/>
     </div>
