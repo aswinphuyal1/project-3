decb1643fa80fb0c2df6b9222fb3ec8bdda00807
aswin <phuyalaswin651@gmail.com>
2025-12-19 10:47:45 +0545

    new page upload created

--- frontend/components/SideNavbar.tsx
+++ frontend/components/SideNavbar.tsx
@@ -27,6 +27,7 @@ const navItems: NavItem[] = [
   { id: 4, label: "Live Sessions", icon: Video, path: "/live" },
   { id: 5, label: "AI Assistant", icon: Brain, path: "/ai" },
   { id: 6, label: "My Profile", icon: User, path: "/profile" },
+  { id: 6, label: "Upload", icon: NotebookText, path: "/upload" },
   { id: 7, label: "Settings", icon: Settings, path: "/setting" },
 ];
 
