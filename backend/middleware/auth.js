0abc1b2b7c2c639968d8577d5a9b95a974874154
aswin <phuyalaswin651@gmail.com>
2025-12-21 21:29:23 +0545

    done with middleware of auth

---
+++ backend/middleware/auth.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+const authuser = async (req, res, next) => {
+  const { token } = req.header;
+  if (!token) {
+    return res.json({
+      success: false,
+      message: "you are not authorize",
+    });
+  }
+  try {
+    const tokendedcode = await jwt.verify(token, process.env.JWT_SECRET);
+req.body.userid=tokendedcode.id
+next();
+
+} catch (error) {
+    console.log(error);
+    res.json({
+        success:false,
+        message:error.message
+    })
+}
+};
+export default authuser
