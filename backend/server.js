858ac48b32d47eb9e6d37f28874e5e6520580e16
aswin <phuyalaswin651@gmail.com>
2025-12-21 17:50:44 +0545

    callled connectcloudinary() in server.js

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,14 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-
+import connectcloudinary from "./config/cloudinary";
 
 //app config
 const app = express();
 const port = process.env.PORT || 4000;
 
 ;
-
+connectcloudinary();
 //middlewares
 app.use(express.json());
 app.use(cors());
