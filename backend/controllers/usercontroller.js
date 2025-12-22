9783fbade7e3356360f530f286c2b2b468ce74be
aswin <phuyalaswin651@gmail.com>
2025-12-22 10:59:18 +0545

    loginuser

--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -1,1 +1,34 @@
 import validator from "validator";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import usermodel from "../models/usermodel";
+const createtoken =(id)=>
+{
+    return jwt.sign({ id }, process.env.JWT_SECRET
+    );
+}
+ const loginuser= async(req,res)=>
+ {
+    try {
+        const{email,password}=req.body
+        const user=usermodel.findOne({email});
+        if(!user)
+        {
+            res.json({success:false,message:"user donot exist create account"});
+
+        }
+        const ismatch= await bcrypt.compare(password,user.password);
+        if(ismatch)
+        {
+            const token=createtoken(user._id)
+            res.json({success:true,token})
+        }
+        else{
+            res.json({ success: true, message:"incorrect password" });
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message})
+    }
+ }
+ 
