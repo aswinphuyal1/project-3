b3c51c2151ecf18ba4706261538ccae089128f83
aswin <phuyalaswin651@gmail.com>
2025-12-22 13:11:33 +0545

    userrouter done

--- backend/routes/userroute.js
+++ backend/routes/userroute.js
@@ -1,1 +1,7 @@
 import express from "express"
+import { loginuser,registeruser,adminlogin } from "../controllers/usercontroller"
+const userrouter=express.Router();
+userrouter.post('/register',registeruser)
+userrouter.post('/login',loginuser);
+userrouter.post('./admin',adminlogin);
+export default userrouter
