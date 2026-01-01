ecc1fae510848116b586fdeb874a6ea7b517b727
aswin <phuyalaswin651@gmail.com>
2026-01-01 11:29:46 +0545

    adminAuth

--- backend/middleware/adminauth.js
+++ backend/middleware/adminauth.js
@@ -1,6 +1,24 @@
-import jwt from "jsonwebtoken"
-const adminaauth= async(
-    {
-        
+import jwt from "jsonwebtoken";
+
+const adminAuth = async (req, res, next) => {
+    try {
+        const { token } = req.headers;
+        if (!token) {
+            return res.json({ success: false, message: "Not Authorized (Admin)" });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        if (decoded.role !== "admin" || decoded.email !== process.env.ADMIN_EMAIL) {
+            return res.json({ success: false, message: "Not Authorized (Admin)" });
+        }
+
+        req.adminEmail = decoded.email;
+        next();
+    } catch (error) {
+        console.error("Admin Auth Error:", error);
+        res.json({ success: false, message: error.message });
     }
-)
+};
+
+export default adminAuth;
