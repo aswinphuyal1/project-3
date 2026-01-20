afbc1ef7a1d1963dcd9966c931e87cc3ca588b80
Aswin phuyal <phuyalaswin651@gmail.com>
2026-01-20 20:52:57 +0545

    error check

--- frontend/app/(root)/AI/page.tsx
+++ frontend/app/(root)/AI/page.tsx
@@ -1,10 +1,11 @@
+
 import AI_Assistant from "@/components/AI_Assistant";
 import React from "react";
 
 const page = () => {
   return (
     <div className="min-h-screen w-full flex flex-col">
-      hi
+    <AI_Assistant/>
     </div>
   );
 };
